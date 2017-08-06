4224af2040f81c4fa5890358415ae509fbd8c863
Adrian Biedrzycki <a@modules.io>
2017-08-05 19:32:47 -0700

    Add more crds/vrid constructor args

--- index.js
+++ index.js
@@ -3,25 +3,33 @@ const path = require('path');
 const crds = require('crds');
 const vrid = require('vrid');
 
-const zeoBlockchain = ({
-  dataDirectory = path.join(__dirname, 'data'),
-} = {}) => Promise.all([
-  crds({dataDirectory}).listen(),
-  vrid({dataDirectory}).listen(),
-])
-  .then(([
-    crdsResult,
-    vridResult,
-  ]) => {
-    console.log(vridResult);
-  })
-  .catch(err => {
-    console.warn(err);
-    process.exit(1);
-  });
+const zeoBlockchain = (opts = {}) => Promise.all([
+  crds(opts).listen(),
+  (() => {
+    const v = vrid(opts);
+
+    if (opts.app) {
+      return v.mountApp();
+    } else {
+      return v.listen();
+    }
+  })(),
+]);
 
 module.exports = zeoBlockchain;
 
 if (!module.parent) {
-  zeoBlockchain();
+  zeoBlockchain({
+    dataDirectory: path.join(__dirname, 'data'),
+  })
+    .then(([
+      crdsResult,
+      vridResult,
+    ]) => {
+      console.log(vridResult);
+    })
+    .catch(err => {
+      console.warn(err);
+      process.exit(1);
+    });
 }
