4ca0a14ca75b276ec1ae83ee7b4a2cdac9adbf0c
Adrian Biedrzycki <a@modules.io>
2017-08-08 00:13:48 -0700

    Refactor start/exit flow

--- index.js
+++ index.js
@@ -3,30 +3,58 @@ const path = require('path');
 const crds = require('crds');
 const vrid = require('vrid');
 
-const zeoBlockchain = (opts = {}) => Promise.all([
-  crds(opts).listen(),
-  (() => {
-    const v = vrid(opts);
+const _requestVrid = opts => {
+  const v = vrid(opts);
 
-    if (opts.app) {
-      return v.mountApp();
-    } else {
-      return v.listen();
-    }
-  })(),
-]);
+  if (opts.app) {
+    return v.mountApp();
+  } else {
+    return v.listen();
+  }
+};
+const _requestCrds = opts => {
+  const c = crds(opts);
 
+  return c.listen()
+    .then(destroy => {
+      c.destroy = destroy;
+      return c;
+    })
+    .then(c => {
+      if (opts.cli) {
+        return c.cli()
+          .then(() => c);
+      } else {
+        return c;
+      }
+    });
+};
+
+const zeoBlockchain = (opts = {}) => _requestVrid(opts)
+  .then(v => {
+    console.log('Admin page: ' + v.url);
+
+    return _requestCrds(opts)
+      .then(c => ([
+        c,
+        v,
+      ]));
+  });
 module.exports = zeoBlockchain;
 
 if (!module.parent) {
   zeoBlockchain({
     dataDirectory: path.join(__dirname, 'data'),
+    cli: true,
   })
     .then(([
       crdsResult,
       vridResult,
     ]) => {
-      console.log(vridResult);
+      crdsResult.on('exit', () => {
+        crdsResult.destroy();
+        vridResult.destroy();
+      });
     })
     .catch(err => {
       console.warn(err);
